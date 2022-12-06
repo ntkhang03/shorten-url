f3757c91ae2c4e3b2fcf4223235ef20369931804
NTKhang <nthanhkhang2003@gmail.com>
2022-12-06 14:37:40 +0700

    fine

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const tinyurl = require('tinyurl');
 
 const app = express();
-app.post('/shorten-url', async (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/shorten', async (req, res) => {
 	try {
-		const { options } = req.body;
-		const result = await tinyurl.shorten(options);
+		const url = req.body.url;
+		const result = await tinyurl.shorten(url);
 		res.json({ result, error: null });
 	}
 	catch (e) {
@@ -13,4 +16,31 @@ app.post('/shorten-url', async (req, res) => {
 	}
 });
 
-app.listen(3000, () => console.log('Server started'));
+app.post('/shortenWithAlias', async (req, res) => {
+	try {
+		const url = req.body.url;
+		const alias = req.body.alias;
+		const result = await tinyurl.shortenWithAlias(url, alias);
+		res.json({ result, error: null });
+	}
+	catch (e) {
+		res.json({ result: null, error: e });
+	}
+});
+
+app.post('/resolve', async (req, res) => {
+	try {
+		const url = req.body.url;
+		const result = await tinyurl.resolve(url);
+		res.json({ result, error: null });
+	}
+	catch (e) {
+		res.json({ result: null, error: e });
+	}
+});
+
+app.get('/', async (req, res) => {
+	res.sendFile(__dirname + '/home.html');
+});
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
